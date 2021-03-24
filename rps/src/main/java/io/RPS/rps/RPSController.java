722f54bbc7e932028db610cd979e7fd86e6178a9
daniellevin76 <55661068+daniellevin76@users.noreply.github.com>
2021-03-24 15:09:22 +0100

    post, get, pvp, pve

--- rps/src/main/java/io/RPS/rps/RPSController.java
+++ rps/src/main/java/io/RPS/rps/RPSController.java
@@ -14,22 +14,39 @@ import io.RPS.RPS;
 @RestController
 public class RPSController {
 
-    ArrayList<ResultBean> winnerList = new ArrayList<ResultBean>();
+    ArrayList<ResultBean> pveWinnerList = new ArrayList<ResultBean>();
+    ArrayList<ResultBean> pvpWinnerList = new ArrayList<ResultBean>();
     int round = 0;
    
     
     @RequestMapping(method = RequestMethod.POST, value = "pve")
-    public String postPvE(@RequestParam(defaultValue = "000") String input){
+    public ArrayList<ResultBean> postPvE(@RequestParam(defaultValue = "000") String input){
         round +=1;
         ResultBean result = new ResultBean(round, input.toUpperCase(), RPS.generateRandomValue(), GamePlay.determineWinner(input, RPS.generateRandomValue()));
-       winnerList.add(result);
-      return "done";
+       pveWinnerList.add(result);
+       return getPvE();
+      
     }
     
     @RequestMapping(method = RequestMethod.GET, value = "pve", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ArrayList<ResultBean> getPvE(@RequestParam(defaultValue = "000") String input){
+    public ArrayList<ResultBean> getPvE(){
        
-    return winnerList;
+    return pveWinnerList;
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "pvp")
+    public ArrayList<ResultBean> postPvP(@RequestParam(defaultValue = "000") String frstinput,@RequestParam(defaultValue = "000") String scndinput){
+        round +=1;
+        ResultBean result = new ResultBean(round, frstinput.toUpperCase(), scndinput.toUpperCase(), GamePlay.determineWinner(frstinput, scndinput));
+       pvpWinnerList.add(result);
+
+       return getPvP();
+    }
+    
+    @RequestMapping(method = RequestMethod.GET, value = "pvp", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ArrayList<ResultBean> getPvP(){
+       
+    return pvpWinnerList;
     }
     
    
